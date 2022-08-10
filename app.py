953c5f3873956e84519f644a6fa0997af31cb9dd
Hardik Chauhan <54958082+19BCS4325-Hardik@users.noreply.github.com>
2022-08-10 15:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ if st.button('Predict Probability'):
         win = result[0][1]
         st.header(batting_team + "- " + str(round(win*100)) + "%")
         st.header(bowling_team + "- " + str(round(loss*100)) + "%")
-    except Exception as e:
-        st.header(e)
+    except Exception:
+        st.header("Some Error is there please check your input values")
